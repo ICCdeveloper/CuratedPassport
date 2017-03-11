7b357b8ccde9ff25df9e25e25001ea5cfb19300d
Silicon <silicons@163.com>
2017-03-11 13:56:37 +0800

    first commit

---
+++ CPControllers/CPHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CPHomeViewController.swift
+//  CuratedPassport
+//
+//  Created by ICCorg on 2017/3/11.
+//  Copyright © 2017年 ICCorg. All rights reserved.
+//
+
+import UIKit
+
+class CPHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CPModel/CPDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CPDataModel.swift
+//  CuratedPassport
+//
+//  Created by ICCorg on 2017/3/11.
+//  Copyright © 2017年 ICCorg. All rights reserved.
+//
+
+import UIKit
+
+class CPDataModel: NSObject {
+
+}
