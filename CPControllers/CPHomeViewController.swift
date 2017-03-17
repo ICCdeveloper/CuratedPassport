16143bfba59881a1ca4155f3b806cd6785771d5a
ICCdeveloper <ryan@iccworld.org>
2017-03-17 12:03:49 +0800

    功能调试

--- CPControllers/CPHomeViewController.swift
+++ CPControllers/CPHomeViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class CPHomeViewController: UIViewController {
 
+    @IBOutlet weak var notifyView: UIView!
+    var notiFrView : CycleScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        self.view.backgroundColor = UIColor.brown
+
+        notiFrView = CycleScrollView.init(frame: CGRect.init(x: 0, y: 0, width: self.notifyView.frame.width, height: self.notifyView.frame.height), animationDuration: 1.0)
+        let viewsArr = NSMutableArray.init()
+        let arr = ["Si","Li","Con","Zou"]
+        for _ in 0...arr.count {
+            for si in arr {
+                let label = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: notifyView.frame.width, height: notifyView.frame.height))
+                label.text = NSString.init(format: "%@", si) as String
+                label.textAlignment = .center
+                label.textColor = UIColor.darkText
+                label.font = UIFont.systemFont(ofSize: 20)
+                viewsArr.add(label)
+            }
+        }
+        self.notiFrView.backgroundColor = UIColor.blue
+        self.notiFrView.fetchContentViewAtIndex = { (pageIndex) in
+
+            return viewsArr[pageIndex] as? UIView
+        }
+        
+        self.notiFrView.totalPagesCount = {
+        
+            return viewsArr.count
+        }
+        
+        self.notifyView.addSubview(notiFrView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +48,5 @@ class CPHomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
